a279c3575917b3521591b9b8fa3fd88430d307d5
MelWal05 <melker.wallebom@skola.taby.se>
2022-10-25 11:13:14 +0200

    Initial commit

---
+++ Spel.py
@@ -0,0 +1,23 @@
+import time
+
+def clear():
+    pass
+
+class Monster():
+    def __init__(self, hp, dmg):
+        self.hp = hp
+        self.dmg = dmg
+
+class Spelare():
+    def __init__(self, namn):
+        self.namn = namn
+        self.hp = 50
+        self.dmg = 10
+        self.pengar = 0
+        self.ryggsäck = []
+
+#Starten på den fantastiska resan
+
+namn = input("Hej, Vad vill ud bli kallad? --> ")
+spelare = Spelare(namn)
+
